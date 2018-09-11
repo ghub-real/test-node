7120706575ab82bf8c05cb7d114db69395b85fd4
renu <renudotvenugopal@gmail.com>
2018-09-12 00:02:27 +0100

    webpack loaders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash';
+import './style.css';
+import Data from './data/data.xml';
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  console.log(Data);
 
   return element;
 }
